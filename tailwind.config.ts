2a107c0c12c3e71a9be68cfb61d99b517c5e82ef
ErinBachmann <erin.bachmann@gmx.ch>
2023-09-08 20:06:38 +0200

    Add Hero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,7 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
-
-export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
+module.exports = {
+	content: ['./src/routes/**/*.{svelte,js,ts}'],
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ["lofi"],
 	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'crimson',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
-} satisfies Config;
+};
